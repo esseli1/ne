4f13a12ccfe60c63337c2caacaebb01a7e02dc0b
esseli1 <46043777+esseli1@users.noreply.github.com>
2018-12-30 20:50:23 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'process.env.BOT_TOKEN'; // حط توكن البوت هنا
+exports.TOKEN = 'NTI4OTk1MjM0NTAwMDUwOTY2.DwqYzw.8XlHRGlrjVQv8vSlTIvOGJsDXlQ'; // حط توكن البوت هنا
 
-exports.PREFIX = '2';//برفكس البوت هو يلي بيشغل لاغاني
+exports.PREFIX = '5';//برفكس البوت هو يلي بيشغل لاغاني
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
